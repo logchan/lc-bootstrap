0f16b9f7b9eef9afedbd1776b69710dce8e17811
logchan <logchan@outlook.com>
2019-05-22 22:01:01 -0400

    add docker

---
+++ ubuntu/extra/docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt install -y \
+                    apt-transport-https \
+                    gnupg-agent \
+                    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
