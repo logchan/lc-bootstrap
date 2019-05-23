32a963b622321ad75443ccb1c68afc7c1a93dcfe
logchan <logchan@outlook.com>
2019-05-22 21:04:25 -0400

    updates

--- centos/disable-selinux.sh
+++ centos/disable-selinux.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+sudo setenforce 0
 sudo mv /etc/selinux/config /etc/selinux/config.bck
-sudo cat /etc/selinux/config.bck | sed 's/SELINUX=enforcing/SELINUX=disabled/' > /etc/selinux/config
+sudo sh -c 'cat /etc/selinux/config.bck | sed s/SELINUX=enforcing/SELINUX=disabled/ > /etc/selinux/config'
